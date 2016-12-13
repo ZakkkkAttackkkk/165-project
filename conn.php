4edb166b64af603dca045d6228907601830f7044
ZakkkkAttackkkk <zakjimenez@outlook.com>
2016-12-13 22:32:31 +0800

    clear files for revamp

--- conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-#session_start();
-$conn = new mysqli("localhost","clientlogin","clientw3w");
-$q = $conn->query("use rainbow;");
-?>
--- help.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "header.php"
-?>
-help.php
-<?php
-    if (isset($_SESSION["login"])){
-        ## stuff
-    }
-?>
